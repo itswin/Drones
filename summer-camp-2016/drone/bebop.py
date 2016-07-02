bc782cb333b8a144ac864375bd7cc02af201eb96
Winston <winston.cheung1@gmail.com>
2016-07-01 23:40:57 -0400

    Added attributes

--- summer-camp-2016/drone/bebop.py
+++ summer-camp-2016/drone/bebop.py
@@ -106,6 +106,12 @@ class Bebop:
         self.barcodes = []
         self.frameWidth = 0
         self.frameHeight = 0
+        self.pictureBoolean = False
+        self.findSphero = False
+        self.lastFrame = None
+        self.moveScaler = 0
+        self.objectCenterX = 0
+        self.objectCenterY = 0
         if self.jpegStream:
             self.videoFrameProcessor = VideoFrames(onlyIFrames=False, verbose=False)
             self.reader = JpegReader(self, self.fps)
